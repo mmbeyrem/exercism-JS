67d9bbb48924880a3df4fd82f7d4b1f0d6787c2f
Beyrem <mmakhlouf@ecovadis.com>
2019-08-08 20:21:15 +0100

    update space-age solution

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -1,20 +1,16 @@
 'use strict'
 
-const converter = (age, coef) => +(age / coef).toFixed(2);
-
-const mapper = new Map([
-  ['earth', (age) => converter(age , 31557600)],
-  ['mercury', (age) => converter(age, (31557600 * 0.2408467))],
-  ['venus', (age) => converter(age, (31557600 * 0.61519726))],
-  ['mars', (age) => converter(age, (31557600 * 1.8808158))],
-  ['jupiter', (age) => converter(age, (31557600 * 11.862615))],
-  ['saturn', (age) => converter(age, (31557600 * 29.447498))],
-  ['uranus', (age) => converter(age, (31557600 * 84.016846))],
-  ['neptune', (age) => converter(age, (31557600 * 164.79132))],
+const OrbitalPeriod = new Map([
+  ['earth', 1],
+  ['mercury', 0.2408467],
+  ['venus', 0.61519726],
+  ['mars', 1.8808158],
+  ['jupiter', 11.862615],
+  ['saturn', 29.447498],
+  ['uranus', 84.016846],
+  ['neptune', 164.79132],
 ])
+const EarthOrbitalPeriodInS = 31557600;
+const AgeOnEarth = (age) => age / EarthOrbitalPeriodInS;
 
-export const age = (planet, age) => {
-  return mapper.get(planet)(age);
-};
-
-
+export const age = (planet, age) => new Number((AgeOnEarth(age) / OrbitalPeriod.get(planet)).toFixed(2));
